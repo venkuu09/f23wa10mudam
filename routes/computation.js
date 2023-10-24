93a2047e93d8ffd28ba0465bca267eb12e43666b
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-10-24 18:47:03 -0500

    Added Bonus Computation

--- routes/computation.js
+++ routes/computation.js
@@ -1,18 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-let randomValue = Math.random() * 100;
-  let x = randomValue;
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // let randomValue = Math.random() * 100;
-  // let x = randomValue;
+  let randomNumber = Math.floor(Math.random()) * 100;
+  let x = randomNumber;
 
-  if (req.query.x) {
-    // If a query argument 'x' is provided, use that value instead
-    x = parseFloat(req.query.x);
-  }
+  // if (req.query.x) {
+  //   // If a query argument 'x' is provided, use that value instead
+  //   x = parseFloat(req.query.x);
+  // }
   const result = Math.round(x);
   const responseText = `Math.round() applied to ${x} is ${result}`;
   res.send(responseText);
