344f2dcae3e4592b6658094b33bf5897ed7ca834
venkuu09 <mailtovenkateshpatel@gmail.com>
2023-10-24 15:24:49 -0500

    Added functionality of Sum to Users.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+let sum = 0;
+let more = 0;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more += 1;
+  sum += more;
+  res.send(`Sum is: ${sum}`);
+  // res.send('respond with a resource');
 });
 
 module.exports = router;
